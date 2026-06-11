a66af34aacaed65392c1e86ba36d95f81ea421c4
mzjoel <mzhssn313@gmail.com>
2026-06-11 23:27:17 +0700

    fix:changes DB_PORT to DB_MON_PORT on monitoringDB.js

--- services/app-service/src/config/monitoringDb.js
+++ services/app-service/src/config/monitoringDb.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const monitoringPool =
   process.env.DB_MON_NAME || process.env.MONITORING_DB_NAME
     ? new Pool({
-        host: process.env.DB_HOST || "localhost",
+        host: process.env.DB_MON_HOST || "localhost",
         port: Number(process.env.DB_MON_PORT || 5433),
         user: process.env.DB_USER || "postgres",
         password: process.env.DB_PASSWORD || "postgres",
